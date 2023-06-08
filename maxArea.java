fd30314b2250210fba7678762f27672a65233f4c
NUsakhyatha <tammineni.y@northeastern.edu>
2023-06-08 10:33:36 -0400

    Commit contains with the mostwater problem

---
+++ maxArea.java
@@ -0,0 +1,23 @@
+public class maxArea {
+    public int maxArea(int[] height) {
+        int max=0;
+        int l=0;int r=height.length-1;
+        while(l<r)
+        {
+            max = Math.max(max,Math.min(height[l],height[r])*(r-l));
+            if(height[l]<=height[r])
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+
+        return max;
+
+    }
+}
+// time complexity : O(n)
+//space complexity O(1)
